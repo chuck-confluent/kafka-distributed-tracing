fbe122bfcdd7102e2888201e323e7d733938e717
Chuck <chuck@confluent.io>
2021-11-15 17:47:27 +0000

    comment kstreams metrics reporter todos

--- kstream-service/src/main/java/myapps/MetricsReporter.java
+++ kstream-service/src/main/java/myapps/MetricsReporter.java
@@ -1,40 +1,68 @@
 package myapps;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
+import javax.management.JMException;
+
+import java.util.function.Predicate;
+
 import org.apache.kafka.common.MetricName;
 import org.apache.kafka.common.Metric;
 import org.apache.kafka.streams.KafkaStreams;
+
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.common.AttributesBuilder;
 import io.opentelemetry.api.metrics.GlobalMeterProvider;
 import io.opentelemetry.api.metrics.Meter;
 
+/**
+ * DRAFT
+ * Report Kafka Streams metrics to observability backend via OpenTelemetry protocol (OTLP)
+ * 
+ * See https://github.com/confluentinc/jmx-monitoring-stacks/blob/6.1.0-post/shared-assets/jmx-exporter/kafka_streams.yml
+ * for an opinionated set of good Kafka Streams JMX metrics to monitor.
+ * 
+ * See see https://docs.confluent.io/platform/current/streams/monitoring.html
+ * for all Kafka Streams metrics
+ */
+
 public class MetricsReporter {
 
-    private static final Meter meter = GlobalMeterProvider.get().get("io.opentelemetry.metrics.hello");
+    public static void reportMetrics(KafkaStreams streams) throws JMException {
 
-    public void reportMetrics(KafkaStreams streams) throws InterruptedException {
+        // Create OpenTelemetry meter
+        Meter meter = GlobalMeterProvider.get().get(MetricsReporter.class.getName());
 
-        while(true){
+        // Create collection of Kafka Streams metrics
+        Collection<? extends Metric> metrics = streams.metrics().values();
 
-            // Access Kafka Streams metrics
-            Map<MetricName, ? extends Metric> metrics = streams.metrics();
+        // TODO Filter metrics collection down to a set that really matter to monitor
 
-            for (Map.Entry<MetricName, ? extends Metric> metric: metrics.entrySet()){
+        // TODO Create collection of metrics of type long
+        Predicate<? extends Metric> isLongMetric = metric -> metric.metricValue().getClass().isInstance(long.class);
+        Collection<? extends Metric> longMetrics = metrics.stream.filter(isLongMetric).collect;
 
-                System.out.println("Metric: " + metric.getKey().toString() + ", " + metric.getValue().metricValue());
-                // meter
-                //     .counterBuilder(metric.getKey().name())
-                //     .setUnit(metric.getValue().toString())
-                //     .buildWithCallback(
-                //         r -> {
-                //             r.observe(metric.getValue().metricValue());
-                //     });
-                TimeUnit.MILLISECONDS.sleep(500);
-            }
+        // TODO Create collections of metrics of other types
 
-            TimeUnit.SECONDS.sleep(30);
-
+        // Observe metrics of type long with the OpenTelemetry API
+        for (Metric longMetric : longMetrics) {
+            MetricName metricName = longMetric.metricName();
+            AttributesBuilder attributesBuilder = Attributes.builder();
+            metricName.tags().forEach((key, value) -> attributesBuilder.put(key, value));
+            Attributes attributes = attributesBuilder.build();
+            meter
+                .counterBuilder(metricName.group() + "_" + metricName.name()) // TODO choose the type of the metric: counter, upDownCounter, gauge, or histogram 
+                .setDescription(metricName.description())
+                .buildWithCallback(observableLongMeasurement -> {
+                    Long metricValue = (Long) longMetric.metricValue(); // TODO verify it's the right way to convert metricValue() into a Long or Double
+                    observableLongMeasurement.observe(metricValue, attributes);
+                });
         }
+
+        // TODO Observe other metrics with the OpenTelemetry API
+
     }
 
     
